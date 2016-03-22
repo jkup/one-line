4dd69c8bfd208f9bd4e425ae34c474ddc49b348c
Jon Kuperman <jon.kuperman@gmail.com>
2016-03-21 23:34:27 -0700

    #3, turning it into a web app

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const cool = require('cool-ascii-faces')
 const funky_intro = require('funny-words')
+const express = require('express')
+
+const app = express()
 
 console.log(cool())
 console.log(funky_intro("Welcome to jkup, a Node.js app where you can only contribute one line at a time. Inspired by jkup himself."))
