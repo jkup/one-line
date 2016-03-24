747ca46ded70c69228e5622387d9f775016d4f09
Mike Ralphson <mike.ralphson@gmail.com>
2016-03-24 11:40:53 +0000

    Listen for http requests. Default port 3000

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ app.get('/', function(req, res) {})
 
 console.log(cool())
 console.log(funky_intro("Welcome to jkup, a Node.js app where you can only contribute one line at a time. Inspired by jkup himself."))
+
+app.listen(process.env.PORT || 3000, function () {});
