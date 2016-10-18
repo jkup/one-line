81bc38683e395ef780ec16a9aebe02bdcedf6bf8
samuel teixeira <samuelteixeiras@gmail.com>
2016-10-18 15:38:15 +0100

    #6- Add oneline functionality

--- index.js
+++ index.js
@@ -5,29 +5,14 @@ const emoji = require('emoji-cool');
 
 const app = express()
 
-
-
-
-
- 
-
 app.get('/', function(req, res) {
-
-  
-
   res.send('Hello Mars! <br> Days until next year:' + Math.round(( new Date().setFullYear(new Date().getFullYear() + 1, 0, 1) - new Date() )/(1000 * 60 * 60 * 24)));
 })
-
 
 app.get('/hangUnlessSomeoneElseReturnsFromHere', function(req, res) {}); // To make this route work, someone else needs to return a response
 app.get('/useless', function(req, res){ res.redirect('http://www.theuselessweb.com/'); })
 
 console.log(emoji)
-
-
-
-
-
 console.log(cool())
 console.log(funky_intro("Welcome to jkup, a Node.js app where you can only contribute one line at a time. Inspired by jkup himself."))
 
