abb36c71e52ef97908c3817867489b06f8ca2ff6
scorpionhiccup <sharvil.2009@gmail.com>
2016-10-18 18:49:08 +0530

    adding cool emoji

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cool = require('cool-ascii-faces')
 const funky_intro = require('funny-words')
 const express = require('express')
+const emoji = require('emoji-cool');
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.get('/', function(req, res) {
   res.send('Hello Mars!');
 })
 
+console.log(emoji)
 console.log(cool())
 console.log(funky_intro("Welcome to jkup, a Node.js app where you can only contribute one line at a time. Inspired by jkup himself."))
 
