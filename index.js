d375920e49a2c2806a3e8d93e7a0a65e2d82a32b
Mithun Patel <mithunster@gmail.com>
2016-10-18 08:48:54 -0500

    #6: My one liner hangs the app :p

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.get('/', function(req, res) {
   res.send('Hello Mars!');
 })
 
+app.get('/hangUnlessSomeoneElseReturnsFromHere', function(req, res) {}); // To make this route work, someone else needs to return a response
+
 console.log(emoji)
 console.log(cool())
 console.log(funky_intro("Welcome to jkup, a Node.js app where you can only contribute one line at a time. Inspired by jkup himself."))
