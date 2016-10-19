0d9d68e131b8c462db88ca52d4b9360eef137e39
Brian Harrington <bdharrington7@users.noreply.github.com>
2016-10-18 20:14:02 -0700

    Save server in a var so we can access it later

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ console.log(emoji)
 console.log(cool())
 console.log(funky_intro("Welcome to jkup, a Node.js app where you can only contribute one line at a time. Inspired by jkup himself."))
 
-app.listen(process.env.PORT || 3000, function () {});
+var server = app.listen(process.env.PORT || 3000, function () {}); // now we can close it if needed
 app.use(express.static('public'));
 console.log("I like this idea. :p");
