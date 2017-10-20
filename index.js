b00639645a048f326a57a27b8657d6745c46768d
Jaken Herman <jch053@shsu.edu>
2017-10-19 22:38:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/duckduckgo/:search', function(req, res) { res.redirect('https://duckdu
 app.get('/beAwesome', function(req, res){ res.redirect('https://github.com/sindresorhus/awesome'); })
 app.get('/motivateMe', function(req, res) {res.redirect('https://www.brainyquote.com/quotes/topics/topic_motivational.html')});
 app.get('/cloud2butt/:text', function(req, res) {res.send(req.params.text.toLowerCase().split('cloud').join('butt'));});
-
+//51 lines in and NOBODY has commented??? Shame. Well, hello everyone and happy Hacktoberfest. Have fun!
 app.use(function (req, res, next) { res.status(404); res.render('404'); });
 
 console.log(emoji)
